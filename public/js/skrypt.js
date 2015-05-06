4b18f2cf9c9c3945b1531fb20390c68991608d7f
Adam Nadoba <anadoba@windowslive.com>
2015-05-06 13:13:15 +0200

    WIP

--- public/js/skrypt.js
+++ public/js/skrypt.js
@@ -1,8 +1,64 @@
 /* jshint browser: true, globalstrict: true, devel: true */
 /* global io: false */
+/* global $: false */
 "use strict";
 
 // Inicjalizacja
+
+// jQuery
+$(document).ready(function () {
+    var socket;
+    
+    // Stan wejściowy
+    $('#status').text = "Brak połączenia";
+    $('#close').prop("disabled", true);
+    $('#send').prop("disabled", true);
+    
+    // Po kliknięciu guzika „Połącz” tworzymy nowe połączenie WS
+    $('#open').click(function () {
+        $('#open').prop("disabled", true);
+        if (!socket || !socket.connected) {
+            socket = io({forceNew: true});
+        }
+        socket.on('connect', function () {
+            $('#close').prop("disabled", false);
+            $('#send').prop("disabled", false);
+            $('#status').prop("src", "img/bullet_green.png");
+            console.log('Nawiązano połączenie przez Socket.io');
+        });
+        socket.on('disconnect', function () {
+            $('#open').prop("disabled", false);
+            $('#status').prop("src", "img/bullet_red.png");
+            console.log('Połączenie przez Socket.io zostało zakończone');
+        });
+        socket.on("error", function (err) {
+            $('#message').text("Błąd połączenia z serwerem: '" + JSON.stringify(err) + "'");
+        });
+        socket.on("echo", function (data) {
+            $('#message').text("Serwer twierdzi, że otrzymał od Ciebie: '" + data + "'");
+        });
+    });
+    
+    // Zamknij połączenie po kliknięciu guzika „Rozłącz”
+    $('#close').click(function () {
+        $('#close').prop("disabled", true);
+        $('#send').prop("disabled", true);
+        $('#open').prop("disabled", false);
+        $('#message').text = "";
+        socket.io.disconnect();
+        console.dir(socket);
+    });
+    
+    // Wyślij komunikat do serwera po naciśnięciu guzika „Wyślij”
+    $('#send').click(function () {
+        socket.emit('message', $('#text').val());
+        console.log('Wysłałem wiadomość: ' + $('#text').val());
+        $('#text').val("");
+    });
+});
+
+/*
+// DOM API
 window.addEventListener("load", function (event) {
     var status = document.getElementById("status");
     var open = document.getElementById("open");
@@ -58,3 +114,4 @@ window.addEventListener("load", function (event) {
         text.value = "";
     });
 });
+*/
